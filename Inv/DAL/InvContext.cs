47085993a1deb8eba6b3818ccf56945bbc6af3c9
Windel Pelayo <support@systemscore.com.ph>
2019-10-17 15:19:26 +0800

    Add project files.

---
+++ Inv/DAL/InvContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web.Configuration;
+using Inv.Models;
+
+namespace Inv.DAL
+{
+    public class InvContext : DbContext
+    {
+
+        public InvContext() : base("InvContext")
+        {
+        }
+
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Product> Products{ get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Inv/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inv.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
